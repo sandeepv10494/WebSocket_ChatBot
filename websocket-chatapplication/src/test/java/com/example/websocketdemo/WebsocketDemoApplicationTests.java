95d057854501f083cda16cb04dacc5ac06dcdbff
Sandeep V <sandeepv.10494@gmail.com>
2019-09-11 15:55:28 +0530

    commit for errors

--- websocket-chatapplication/src/test/java/com/example/websocketdemo/WebsocketDemoApplicationTests.java
+++ websocket-chatapplication/src/test/java/com/example/websocketdemo/WebsocketDemoApplicationTests.java
@@ -5,8 +5,10 @@ import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import com.sv.websocket.WebsocketChatApplication;
+
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(classes={WebsocketChatApplication.class})
 public class WebsocketDemoApplicationTests {
 
 	@Test
